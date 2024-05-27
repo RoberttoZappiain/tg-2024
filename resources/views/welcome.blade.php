5489673051684512ebd10418297b2754e7cf6d6b
Robertto Zappiain <roberttozappiain@gmail.com>
2024-05-27 02:06:50 -0700

    navbar details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,104 +17,8 @@
         <div class="spinner"></div>
     </div>
     <div id="app">
-        @yield('navbar')
-        <div class="d-flex justify-content-around">
-                <div class="p-2 text-center lh-1 mt-2 mb-1 p-2">                
-                    <span class="fw-medium">Calle Volcán Vesubio #5145, El Collí Urbano, 45070, Zapopan, Jalisco.</span>
-                </div>
-                <div class="d-flex flex-row">
-                    <div class="p-2">
-                        <div class="mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="25px" height="25px"
-                                class="icon icon-facebook">
-                                <path
-                                    d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z" />
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="p-2">
-                        <div class="mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="25px" height="25px"
-                                class="icon icon-instagram">
-                                <path
-                                    d="M194.4 211.7a53.3 53.3 0 1 0 59.3 88.7 53.3 53.3 0 1 0 -59.3-88.7zm142.3-68.4c-5.2-5.2-11.5-9.3-18.4-12c-18.1-7.1-57.6-6.8-83.1-6.5c-4.1 0-7.9 .1-11.2 .1c-3.3 0-7.2 0-11.4-.1c-25.5-.3-64.8-.7-82.9 6.5c-6.9 2.7-13.1 6.8-18.4 12s-9.3 11.5-12 18.4c-7.1 18.1-6.7 57.7-6.5 83.2c0 4.1 .1 7.9 .1 11.1s0 7-.1 11.1c-.2 25.5-.6 65.1 6.5 83.2c2.7 6.9 6.8 13.1 12 18.4s11.5 9.3 18.4 12c18.1 7.1 57.6 6.8 83.1 6.5c4.1 0 7.9-.1 11.2-.1c3.3 0 7.2 0 11.4 .1c25.5 .3 64.8 .7 82.9-6.5c6.9-2.7 13.1-6.8 18.4-12s9.3-11.5 12-18.4c7.2-18 6.8-57.4 6.5-83c0-4.2-.1-8.1-.1-11.4s0-7.1 .1-11.4c.3-25.5 .7-64.9-6.5-83l0 0c-2.7-6.9-6.8-13.1-12-18.4zm-67.1 44.5A82 82 0 1 1 178.4 324.2a82 82 0 1 1 91.1-136.4zm29.2-1.3c-3.1-2.1-5.6-5.1-7.1-8.6s-1.8-7.3-1.1-11.1s2.6-7.1 5.2-9.8s6.1-4.5 9.8-5.2s7.6-.4 11.1 1.1s6.5 3.9 8.6 7s3.2 6.8 3.2 10.6c0 2.5-.5 5-1.4 7.3s-2.4 4.4-4.1 6.2s-3.9 3.2-6.2 4.2s-4.8 1.5-7.3 1.5l0 0c-3.8 0-7.5-1.1-10.6-3.2zM448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM357 389c-18.7 18.7-41.4 24.6-67 25.9c-26.4 1.5-105.6 1.5-132 0c-25.6-1.3-48.3-7.2-67-25.9s-24.6-41.4-25.8-67c-1.5-26.4-1.5-105.6 0-132c1.3-25.6 7.1-48.3 25.8-67s41.5-24.6 67-25.8c26.4-1.5 105.6-1.5 132 0c25.6 1.3 48.3 7.1 67 25.8s24.6 41.4 25.8 67c1.5 26.3 1.5 105.4 0 131.9c-1.3 25.6-7.1 48.3-25.8 67z" />
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="p-2">
-                        <div class="mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="25px" height="25px"
-                                class="icon icon-youtube">
-                                <path
-                                    d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="p-2">
-                        <div class="mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="25px" height="25px"
-                                class="icon icon-twitter"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                <path
-                                    d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM351.3 199.3v0c0 86.7-66 186.6-186.6 186.6c-37.2 0-71.7-10.8-100.7-29.4c5.3 .6 10.4 .8 15.8 .8c30.7 0 58.9-10.4 81.4-28c-28.8-.6-53-19.5-61.3-45.5c10.1 1.5 19.2 1.5 29.6-1.2c-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3c-9-6-16.4-14.1-21.5-23.6s-7.8-20.2-7.7-31c0-12.2 3.2-23.4 8.9-33.1c32.3 39.8 80.8 65.8 135.2 68.6c-9.3-44.5 24-80.6 64-80.6c18.9 0 35.9 7.9 47.9 20.7c14.8-2.8 29-8.3 41.6-15.8c-4.9 15.2-15.2 28-28.8 36.1c13.2-1.4 26-5.1 37.8-10.2c-8.9 13.1-20.1 24.7-32.9 34c.2 2.8 .2 5.7 .2 8.5z" />
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="p-2">
-                        <div class="mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="25px" height="25px"
-                                class="icon icon-linkedin">
-                                <path
-                                    d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
-                            </svg>
-                        </div>
-                    </div>
-                </div>                               
-        </div>
+        @include('navbar')
 
-        {{-- <div class="d-flex flex-column mb-3">
-            <div class="p-2 text-center lh-1 mt-2 mb-1 p-2">                
-                <span class="fw-medium">Calle Volcán Vesubio #5145, El Collí Urbano, 45070, Zapopan, Jalisco.</span>
-            </div>
-            <div class="p-2">Flex item 2</div>
-            <div class="p-2">Flex item 3</div>
-        </div> --}}        
-        <div class="bgII d-flex  justify-content-around"
-            style="height: 100px">
-            <img src="{{ asset('media/logo-tg-lg.webp')}}" class="rounded h-100 img-fluid "
-            alt="logo total vertical">
-            <div class="d-flex flex-column py-1">
-                <ul class="list-group list-group-horizontal">
-                    <a href="#" class="text-links">Empresa</a>
-                    <a class="text-links">Capacitaciónes</a>
-                    <a class="text-links">Eventos</a>
-                    <a class="text-links">Multimedia</a>
-                    <a class="text-links btn-contact sh-r rounded">CONTACTANOS</a>
-                  </ul>
-                <div class="p-2">
-                    <ul class="list-group list-group-horizontal">
-                        <li class="btn btn-dark dropdown-toggle custom-dropdown bg-transparent border-0" data-bs-toggle="dropdown" aria-expanded="false">
-                            Servicios
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="dropdown-icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
-                        </li>
-                          
-                         <ul class="dropdown-menu dropdown-menu-dark">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                          </ul>
-                        <li class="btn btn-dark dropdown-toggle custom-dropdown bg-transparent border-0" data-bs-toggle="dropdown" aria-expanded="false">Productos
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="dropdown-icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
-                        </li>
-                        <ul class="dropdown-menu dropdown-menu-dark">
-                            <li><a class="dropdown-item" href="#">Actions</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                          </ul>
-                        <li class="text-links">Manuales</li>                        
-                      </ul>
-                </div>
-              </div>
-        </div>
     </div>
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
@@ -124,6 +28,45 @@
             window.addEventListener('load', function() {
                 var preloader = document.getElementById('preloader');
                 preloader.style.display = 'none';
+            });
+        </script>
+           <script>
+            document.querySelectorAll('.accordion-button').forEach(button => {
+                button.addEventListener('click', function(e) {
+                    e.stopPropagation(); // Evita que el menú desplegable se cierre
+
+                    const icon = this.querySelector('.dropdown-icon');
+                    const allIcons = document.querySelectorAll('.accordion-button .dropdown-icon');
+
+                    allIcons.forEach(i => {
+                        if (i !== icon) {
+                            i.classList.remove('open');
+                        }
+                    });
+
+                    icon.classList.toggle('open');
+
+                    // Colapsar otros elementos del acordeón
+                    document.querySelectorAll('.accordion-collapse').forEach(collapse => {
+                        if (collapse !== document.querySelector(this.getAttribute('data-bs-target'))) {
+                            collapse.classList.remove('show');
+                        }
+                    });
+                });
+            });
+
+            // Evita que el menú se cierre cuando se hace clic dentro del acordeón
+            document.querySelectorAll('.accordion-collapse').forEach(collapse => {
+                collapse.addEventListener('click', function(e) {
+                    e.stopPropagation();
+                });
+            });
+
+            // Evita que el menú se cierre cuando se hace clic en cualquier parte del acordeón
+            document.querySelectorAll('.accordion-body').forEach(body => {
+                body.addEventListener('click', function(e) {
+                    e.stopPropagation();
+                });
             });
         </script>
 </body>
